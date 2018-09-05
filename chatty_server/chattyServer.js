06e4fbb8deb3976817a53ad185691d012933cb16
diaaposto <dia.apostolakis@icloud.com>
2018-09-05 20:39:35 +0000

    generating a unique message id on the server - broadcasting the incoming message to all connected clients

--- chatty_server/chattyServer.js
+++ chatty_server/chattyServer.js
@@ -2,6 +2,8 @@
 
 const express = require('express');
 const SocketServer = require('ws').Server;
+// const WebSocket = require('ws');
+const uuidv4 = require('uuid/v4');
 
 // Set the port to 3001
 const PORT = 3001;
@@ -10,7 +12,8 @@ const PORT = 3001;
 const server = express()
    // Make the express server serve static assets (html, javascript, css) from the /public folder
   .use(express.static('public'))
-  .listen(PORT, '0.0.0.0', 'localhost', 
+  .listen(
+      PORT, '0.0.0.0', 'localhost', 
   () => console.log(`Listening on ${ PORT }`));
 
 // Create the WebSockets server
@@ -19,9 +22,31 @@ const wss = new SocketServer({ server });
 // Set up a callback that will run when a client connects to the server
 // When a client connects they are assigned a socket, represented by
 // the ws parameter in the callback.
-wss.on('connection', (ws) => {
+
+//ws param contains the object of the person that just connected to your socket
+wss.on('connection', (client) => {
   console.log('Client connected');
 
+  //Echo back messages (testing purposes)
+  client.on('message', broadcastBack);
+//   console.log('what is this', broadcastBack(message))
+
   // Set up a callback for when a client closes the socket. This usually means they closed their browser.
-  ws.on('close', () => console.log('Client disconnected'));
+  client.on('close', () => console.log('Client disconnected'));
 });
+
+//broadcast - goes through each client and sends message data
+//clients contains an array of everyone who is connected to the socket @ that moment
+wss.broadcast = function(data) {
+    wss.clients.forEach(client =>  {
+        client.send(data);
+    });
+}
+
+function broadcastBack(messageObj) {
+    const msg = JSON.parse(messageObj);
+    msg.id = uuidv4();
+    
+    console.log(`Received: ${JSON.stringify(msg)}`)
+    wss.broadcast(JSON.stringify(msg));
+}
