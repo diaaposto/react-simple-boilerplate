b0d3d9e500cc5b6605ebe00393d7ffb43700327b
diaaposto <dia.apostolakis@icloud.com>
2018-09-10 16:12:33 +0000

    added images to message file in src

--- src/Message.jsx
+++ src/Message.jsx
@@ -1,24 +1,39 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 //change notification for names make a distinction if its a notification or a message
-//transform this into a stateless component
 
 class Message extends Component {
  
   render() {
     const {username, content, colour} = this.props
-    return (
-    <div className="message">
-      <span className="message-username" style={ { color: colour } }>{username}</span>
-      <span className="message-content">{content}</span>
-    </div>
-    );
-  }
+    const regExForImages = /https?:\/\/(~?\w+(-?\w*)+(\/|\.))+(png|jpg|gif)/gi;
+    const image = content.match(regExForImages);
+
+    if (image) {
+      content.replace(image[0],`<img src='${image[0]}' />`) ;
+      
+      return (
+        <div className="message">
+          <span className="message-username" style={ { color: colour } }>{username}</span>
+          <span className="message-content"><img src={image[0]} width="300" alt="image" /></span>
+        </div>
+      );
+      
+      } else {
+        return (
+        <div className="message">
+          <span className="message-username" style={ { color: colour } }>{username}</span>
+          <span className="message-content">{content}</span>
+        </div>
+      );
+    }
+  } 
 }
 
 Message.propTypes = {
   username: PropTypes.string,
   content: PropTypes.string,
-  colour: PropTypes.string
+  colour: PropTypes.string,
+  image: PropTypes.string
 }
 export default Message;
